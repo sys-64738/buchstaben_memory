85f7acc9c280fda78e435f00560bc17febaed1df
Burkhard Meier <burkhard.meier@gmx.de>
2017-11-12 21:23:49 +0100

    Funktioniert jetzt im Prinzip

--- memory.js
+++ memory.js
@@ -3,16 +3,22 @@
 (function () {
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
   var anzahlPaare;
+  var $spielfeld;
+  var $status;
+  var $aufgedeckteKarten = [];
 
   // Nach dem Laden der Seite die Ereignisse binden:
   $(document).ready(function() {
+    $spielfeld  = $("#spielfeld");
+    $status = $("#status");
     $('#los_gehts').on('click', spielStarten);
+    $(document).on('click', '.karte',karteGeklickt);
   });
 
   function spielStarten() {
     anzahlPaare = parseInt( $("#anzahl-paare").val() );    
     if(anzahlPaare < 3 || anzahlPaare > alphabet.length) {
-      $("#status").html("Nicht weniger als 3, nicht mehr als " + alphabet.length + "!");     
+      $status.html("Nicht weniger als 3, nicht mehr als " + alphabet.length + "!");     
     }
     else {
       // Ganzes Alphabet mischen
@@ -34,6 +40,40 @@
       kartenAuslegen(gemischteBuchstaben);
     }
   }  
+  
+  // Karte aufdecken oder wieder zuklappen
+  function karteGeklickt(e) {
+    var buchstabe = $(this).attr('data-buchstabe');
+    var $kartenInhalt = $(this).find('.karten-inhalt');
+    if($kartenInhalt.hasClass('verdeckt')) {
+      if($aufgedeckteKarten.length == 2) {
+        $status.html("Nicht mehr als zwei Karten auf einmal aufdecken!");        
+      }
+      else {
+        $kartenInhalt.removeClass('verdeckt');
+        $kartenInhalt.find('p').removeClass('unsichtbar');
+        $aufgedeckteKarten.push(buchstabe);
+        if($aufgedeckteKarten.length == 2) {
+          if($aufgedeckteKarten[0].toUpperCase() == $aufgedeckteKarten[1].toUpperCase()) {
+            $status.html("Super!");
+            $aufgedeckteKarten = [];
+            if($spielfeld.find('.verdeckt').length == 0) {
+              $status.html("Gewonnen!!! Nochmal?");
+            }
+          } 
+          else {
+            $status.html("Passt nicht, wieder verdecken!");
+          }
+        }
+        
+      }
+    }
+    else {
+      $aufgedeckteKarten.splice($aufgedeckteKarten.indexOf(buchstabe), 1);
+      $kartenInhalt.addClass('verdeckt');
+      $kartenInhalt.find('p').addClass('unsichtbar');
+    }    
+  };
 
     // Ein Array mischen
   function mischen(elemente) {
@@ -47,9 +87,8 @@
 
   // Karten auslegen
   function kartenAuslegen(karten) {
-    $("#status").html("Ausgewählt: " + anzahlPaare + " Paare " + karten.join('') );
+    $status.html("Es geht los mit " + karten.length + " Karten!");
 
-    var $spielfeld  = $("#spielfeld");
     console.log("Breite: " + $spielfeld.width() );
     console.log("Höhe: " + $spielfeld.height() );
     var anzahlSpalten = Math.sqrt(karten.length);
@@ -64,14 +103,13 @@
     $spielfeld.attr('style', 'width: ' + spielfeldBreite + 'px');
     var kartenbreite = Math.floor( spielfeldBreite / anzahlSpalten );   
     var $musterKarte = $("#musterkarte");
-    $musterKarte.attr('style', 'width: ' + kartenbreite + "px; height:" + kartenbreite + 'px;');
-    
-    
+    $musterKarte.attr('style', 'width: ' + kartenbreite + "px; height:" + kartenbreite + 'px;');      
     
     $spielfeld.empty();
     for(var i=0; i < karten.length; i++) {
       $neueKarte = $musterKarte.clone();
       $neueKarte.attr('id', "karte" + i);
+      $neueKarte.attr('data-buchstabe', karten[i]);
       $neueKarte.find("p").html(karten[i]);      
       $neueKarte.removeClass("unsichtbar");
       $spielfeld.append($neueKarte);

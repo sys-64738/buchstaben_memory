e00bea62ff94b062c6fe35961af8a4dfbaf251b7
Burkhard Meier <burkhard.meier@gmx.de>
2017-11-13 20:46:01 +0100

    Memorykarten werden automatisch wieder zugeklappt, und etwas mehr Farbe usw.

--- memory.js
+++ memory.js
@@ -1,18 +1,19 @@
 // Programmcode Buchstabenmemory
 
-(function () {
+// Eine Pseudo-Funktion (IIFE) als Gültigkeitsbereich für unsere Variablen
+(function () { 
   var alphabet = "abcdefghijklmnopqrstuvwxyz";
   var anzahlPaare;
   var $spielfeld;
   var $status;
-  var $aufgedeckteKarten = [];
+  //var $aufgedeckteKarten =  $([]);
 
   // Nach dem Laden der Seite die Ereignisse binden:
   $(document).ready(function() {
     $spielfeld  = $("#spielfeld");
     $status = $("#status");
     $('#los_gehts').on('click', spielStarten);
-    $(document).on('click', '.karte',karteGeklickt);
+    $(document).on('click', '.karten-inhalt.verdeckt', karteGeklickt);
   });
 
   function spielStarten() {
@@ -43,39 +44,53 @@
   
   // Karte aufdecken oder wieder zuklappen
   function karteGeklickt(e) {
-    var buchstabe = $(this).attr('data-buchstabe');
-    var $kartenInhalt = $(this).find('.karten-inhalt');
-    if($kartenInhalt.hasClass('verdeckt')) {
-      if($aufgedeckteKarten.length == 2) {
-        $status.html("Nicht mehr als zwei Karten auf einmal aufdecken!");        
-      }
-      else {
-        $kartenInhalt.removeClass('verdeckt');
-        $kartenInhalt.find('p').removeClass('unsichtbar');
-        $aufgedeckteKarten.push(buchstabe);
-        if($aufgedeckteKarten.length == 2) {
-          if($aufgedeckteKarten[0].toUpperCase() == $aufgedeckteKarten[1].toUpperCase()) {
-            $status.html("Super!");
-            $aufgedeckteKarten = [];
-            if($spielfeld.find('.verdeckt').length == 0) {
-              $status.html("Gewonnen!!! Nochmal?");
-            }
-          } 
-          else {
-            $status.html("Passt nicht, wieder verdecken!");
-          }
-        }
-        
-      }
+    var $karte = $(this);
+
+    var $aufgedeckteKarten = $spielfeld.find('.aufgedeckt');
+
+    if($aufgedeckteKarten.length >= 2) {
+      $status.html("Moment!");
+      return;
     }
-    else {
-      $aufgedeckteKarten.splice($aufgedeckteKarten.indexOf(buchstabe), 1);
-      $kartenInhalt.addClass('verdeckt');
-      $kartenInhalt.find('p').addClass('unsichtbar');
-    }    
+    var buchstabe = $karte.attr('data-buchstabe');
+
+    $karte.removeClass('verdeckt');
+    $karte.addClass('aufgedeckt');
+    $karte.find('p').removeClass('unsichtbar');
+
+    $aufgedeckteKarten = $spielfeld.find('.aufgedeckt');
+    if($aufgedeckteKarten.length == 2) {
+      $k1 = $aufgedeckteKarten.eq(0);
+      $k2 = $aufgedeckteKarten.eq(1);
+      if($k1.attr('data-buchstabe').toUpperCase() 
+          == $k2.attr('data-buchstabe').toUpperCase()) {
+        $status.html("Super!");
+
+        $aufgedeckteKarten.removeClass('aufgedeckt')
+                          .addClass('paar-passt')
+                          .delay(2000).queue(function() {
+          $(this).removeClass('paar-passt');
+          $(this).dequeue();
+        } );
+
+        if($spielfeld.find('.verdeckt').length == 0) {
+          $status.html("Gewonnen!!! Nochmal?");
+          $spielfeld.addClass('paar-passt');
+        }
+      } 
+      else {
+        $aufgedeckteKarten.addClass('paar-passt-nicht')
+                          .delay(2000).queue(function() {
+          $(this).find('p').addClass('unsichtbar');
+          $(this).removeClass('aufgedeckt').removeClass('paar-passt-nicht').addClass('verdeckt');
+          $(this).dequeue();
+        } );
+        $status.html("Passt nicht!");
+      }
+    }      
   };
 
-    // Ein Array mischen
+  // Ein Array mischen
   function mischen(elemente) {
     for(var i=0; i < elemente.length; i++) {
       var zufallsIndex = Math.floor(Math.random()*(i+1)); 
@@ -89,27 +104,30 @@
   function kartenAuslegen(karten) {
     $status.html("Es geht los mit " + karten.length + " Karten!");
 
-    console.log("Breite: " + $spielfeld.width() );
-    console.log("Höhe: " + $spielfeld.height() );
+    // Die Karten sollen soweit möglich in einem Quadrat ausgelegt werden
     var anzahlSpalten = Math.sqrt(karten.length);
     if(Math.floor(anzahlSpalten * 100) == Math.floor(anzahlSpalten) * 100) {
-      anzahlSpalten = Math.floor(anzahlSpalten)
+      anzahlSpalten = Math.floor(anzahlSpalten); // passt genau, z.B. 4x4 Karten
     }
     else {
-      anzahlSpalten = Math.floor(anzahlSpalten) + 1;
+      anzahlSpalten = Math.floor(anzahlSpalten) + 1; // eine weitere Spalte benötigt
     }  
 
     var spielfeldBreite = Math.min($spielfeld.width(), $spielfeld.height() );
     $spielfeld.attr('style', 'width: ' + spielfeldBreite + 'px');
+    
+    // Kartengröße an Spielfeldgröße anpassen:
     var kartenbreite = Math.floor( spielfeldBreite / anzahlSpalten );   
+    
     var $musterKarte = $("#musterkarte");
     $musterKarte.attr('style', 'width: ' + kartenbreite + "px; height:" + kartenbreite + 'px;');      
     
+    // Karten verdeckt auslegen:
     $spielfeld.empty();
     for(var i=0; i < karten.length; i++) {
       $neueKarte = $musterKarte.clone();
       $neueKarte.attr('id', "karte" + i);
-      $neueKarte.attr('data-buchstabe', karten[i]);
+      $neueKarte.find('.karten-inhalt').attr('data-buchstabe', karten[i]);
       $neueKarte.find("p").html(karten[i]);      
       $neueKarte.removeClass("unsichtbar");
       $spielfeld.append($neueKarte);
